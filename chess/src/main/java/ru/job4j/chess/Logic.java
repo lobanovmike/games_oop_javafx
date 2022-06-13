4a93447373faf6807baefbbb07453923968b886c
Mikhail Lobanov <lobanov.m1ke@yandex.ru>
2022-06-13 03:11:03 +0300

    2. Каркас шахматной доски [#792]

--- chess/src/main/java/ru/job4j/chess/Logic.java
+++ chess/src/main/java/ru/job4j/chess/Logic.java
@@ -23,7 +23,7 @@ public final class Logic {
     private boolean free(Cell[] steps) throws OccupiedCellException {
         for (Figure figure : figures) {
             for (Cell step : steps) {
-                if (figure.position() == step) {
+                if (figure != null && figure.position().equals(step)) {
                     throw new OccupiedCellException();
                 }
             }
--- chess/src/test/java/ru/job4j/chess/LogicTest.java
+++ chess/src/test/java/ru/job4j/chess/LogicTest.java
@@ -1,6 +1,5 @@
 package ru.job4j.chess;
 
-import org.junit.Ignore;
 import org.junit.Test;
 import ru.job4j.chess.firuges.Cell;
 import ru.job4j.chess.firuges.Figure;
@@ -8,15 +7,6 @@ import ru.job4j.chess.firuges.black.BishopBlack;
 import ru.job4j.chess.firuges.black.KingBlack;
 
 public class LogicTest {
-
-    @Ignore
-    @Test
-    public void move()
-            throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
-        Logic logic = new Logic();
-        logic.add(new BishopBlack(Cell.C1));
-        logic.move(Cell.C1, Cell.H6);
-    }
 
     @Test(expected = FigureNotFoundException.class)
     public void whenFigureNotFound() throws OccupiedCellException, FigureNotFoundException {
